e2e6056259f80de2aada6338a14076c4258baf2f
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-03 00:48:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(phrase)
-  phrase = "WHAT?"
+  phrase = "Hi, Nana!"
   while phrase == phrase.downcase do
     puts "HUH?! SPEAK UP, SONNY!"
-  if phrase == phrase.upcase
+  while phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
-  else phrase == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
     end
   end
